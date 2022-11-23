3d23067b9605f299657d0405250372a4a00f12e0
njcolvin <njcolvin@live.unc.edu>
2022-11-23 00:41:52 -0500

    move semaphore to coordinator

--- csmc.c
+++ csmc.c
@@ -7,7 +7,7 @@
 
 const int num_arguments = 5;
 pthread_t *s_threads, *t_threads, *c_thread;
-sem_t chair_free, student_waiting, tutor_waiting;
+sem_t chair_free, tutor_ready, student_ready;
 int students, tutors, chairs, help, num_free_chairs;
 
 // generate a random number between 0 and 1 inclusive
@@ -23,56 +23,57 @@ void error(char *message) {
 void *StudentThread(void *threadid) {
     int tid;
     tid = *(int*)threadid;
+    int helped_times = 0;
 
     // student programs for a random interval between 0 and 2ms
     // then checks if there is a free chair in the waiting room
-    while (1) {
+    while (helped_times < help) {
         // sleep for 2ms * random fraction
-        double rand_frac = RandomFraction();
-        printf("student %d sleeping for %lf...\n", tid, 2000 * rand_frac);
-        usleep(2000 * rand_frac);
-        printf("student %d awake from sleeping...\n", tid);
+        usleep(2000 * RandomFraction());
 
         // get num_chair semaphore
         if (sem_wait(&chair_free) != 0)
             error("Error. Failed to wait for chair_free.\n");
 
-        printf("student %d acquired chair_free. num_free_chairs = %d\n", tid, num_free_chairs);
-
         // check num_free_chairs value
         if (num_free_chairs <= 0) {
+            printf("S: Student %d found no empty chair. Will try again later.\n", tid);
+
             if (sem_post(&chair_free) != 0)
                 error("Error. Failed to post chair_free.\n");
         } else {
             num_free_chairs--;        
-            printf("student %d decremented num_free_chairs. num_free_chairs = %d\n", tid, num_free_chairs);
+            printf("S: Student %d takes a seat. Empty chairs = %d.\n", tid, num_free_chairs);
+
             if (sem_post(&chair_free) != 0)
                 error("Error. Failed to post chair_free.\n");
-            break;
+            
+            // student is now in the waiting area
+
+            // let the coordinator know I am waiting
+            if (sem_post(&student_ready) != 0)
+                error("Error. Failed to post student_ready.\n");
+
+            // wait for the tutor to call me
+            if (sem_wait(&tutor_ready) != 0)
+                error("Error. Failed to wait for tutor_ready.\n");
+
+            // increment num_free_chairs
+            // get num_chair semaphore
+            if (sem_wait(&chair_free) != 0)
+                error("Error. Failed to wait for chair_free.\n");
+            printf("student %d acquired chair_free again. num_free_chairs = %d\n", tid, num_free_chairs);
+            num_free_chairs++;
+            printf("student %d incremented num_free_chairs. num_free_chairs = %d\n", tid, num_free_chairs);
+            if (sem_post(&chair_free) != 0)
+                error("Error. Failed to post chair_free.\n");
+
+            // TODO: get tutored
+
+            helped_times++;
+
         }
     }
-
-    // let the tutor know I am waiting
-    if (sem_post(&tutor_waiting) != 0)
-        error("Error. Failed to post tutor_waiting.\n");
-
-    // wait for the tutor to call me
-    if (sem_wait(&student_waiting) != 0)
-        error("Error. Failed to wait for student_waiting.\n");
-
-    // increment num_free_chairs
-    // get num_chair semaphore
-    if (sem_wait(&chair_free) != 0)
-        error("Error. Failed to wait for chair_free.\n");
-    printf("student %d acquired chair_free again. num_free_chairs = %d\n", tid, num_free_chairs);
-    num_free_chairs++;
-    printf("student %d incremented num_free_chairs. num_free_chairs = %d\n", tid, num_free_chairs);
-    if (sem_post(&chair_free) != 0)
-        error("Error. Failed to post chair_free.\n");
-
-    // TODO: get tutored
-    // TODO: update number of times I have been helped
-    // TODO: if I am done being helped, exit, otherwise start over
 
     return 0;
 }
@@ -82,13 +83,9 @@ void *TutorThread(void *threadid) {
     tid = *(int*)threadid;
     printf("Hello World! Tutor Thread ID, %d\n", tid);
 
-    // wait for a student to call me
-    if (sem_wait(&tutor_waiting) != 0)
-        error("Error. Failed to wait for tutor_waiting.\n");
-
     // let the student know I am ready
-    if (sem_post(&student_waiting) != 0)
-        error("Error. Failed to post student_waiting.\n");
+    if (sem_post(&tutor_ready) != 0)
+        error("Error. Failed to post tutor_ready.\n");
 
     // TODO: tutor
     // TODO: start over
@@ -101,7 +98,11 @@ void *CoordinatorThread(void *threadid) {
     tid = *(int*)threadid;
     printf("Hello World! Coordinator Thread ID, %d\n", tid);
 
-    // TODO: everything
+    // wait for a student to call me
+    if (sem_wait(&student_ready) != 0)
+        error("Error. Failed to wait for student_ready.\n");
+
+    
 
     return 0;
 }
@@ -134,11 +135,11 @@ int main(int argc, char *argv[])
     if (sem_init(&chair_free, 0, 1) != 0)
         error("Error. Failed to init chair_free semaphore\n");
 
-    if (sem_init(&student_waiting, 0, 0) != 0)
-        error("Error. Failed to init student_waiting semaphore\n");
+    if (sem_init(&tutor_ready, 0, 0) != 0)
+        error("Error. Failed to init tutor_ready semaphore\n");
 
-    if (sem_init(&tutor_waiting, 0, 0) != 0)
-        error("Error. Failed to init tutor_waiting semaphore\n");
+    if (sem_init(&student_ready, 0, 0) != 0)
+        error("Error. Failed to init student_ready semaphore\n");
 
     int i;
 
